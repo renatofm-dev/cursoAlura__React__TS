04d24206e75743818000f216b6372022e1d354ec
Renato F. Martins <rmartins6694@gmail.com>
2023-04-29 11:37:02 -0300

    reset css and add baseUrl in ts config

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg"
 
 export default function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img src="src\assets\logo.svg" alt="logo aluroni"/>
+        <Logo/>
       </nav>
     </main>
   )
